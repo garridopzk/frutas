08932bbd16ba0f3d2f04d5935297f4f64ebc5938
garridopzk <nictehagarrido45@gmail.com>
2024-05-27 22:07:26 -0600

    cuarto commit

--- app/src/main/java/com/example/frutas/MainActivity.java
+++ app/src/main/java/com/example/frutas/MainActivity.java
@@ -2,18 +2,25 @@ package com.example.frutas;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    ListView listview;
+    private ListView listview;
+    TextView totalTextView;
+    double total = 0.00;
+
+
+
 
 
     @Override
@@ -23,7 +30,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
             listview=(ListView)findViewById(R.id.listview);
-            String[] nombres={"Fresa","Mango","Papaya","Pera","Platanos"};
+            totalTextView = findViewById(R.id.total);
+
+
+        String[] nombres={"Fresa","Mango","Papaya","Pera","Platanos"};
             String[] precios ={"40","50","30","70","90"};
             Integer[]  idimage= {R.drawable.fresa,R.drawable.mango,R.drawable.papaya,R.drawable.pera,R.drawable.platanos};
             Adaptador adapter = new Adaptador(this,nombres,precios,idimage);
@@ -32,12 +42,31 @@ public class MainActivity extends AppCompatActivity {
             listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    double precio = Double.parseDouble(precios[position]);
+                    int imagenResId = idimage[position];
+
+
+                    String precioString = precios[position];
+                    double precio = Double.parseDouble(precioString.replace("$", "").replace(",", ""));
+
+                    total += precio;
+
+                    String totalFormatted = String.format("Total: $%,.2f", total);
+                    totalTextView.setText(totalFormatted);
+
                     Toast.makeText(getApplicationContext(),"Seleccionaste el elemento: "+nombres[position], Toast.LENGTH_LONG).show();
                 }
             });
             listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
                 @Override
                 public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                    String precioString = precios[position];
+                    double precio = Double.parseDouble(precioString.replace("$", "").replace(",", ""));
+
+                    total -= precio;
+
+                    String totalFormatted = String.format("Total: $%,.2f", total);
+                    totalTextView.setText(totalFormatted);
                     Toast.makeText(getApplicationContext(),": "+nombres[position]+"con precio de :$"+precios[position], Toast.LENGTH_LONG).show();
                     return true;
                 }
